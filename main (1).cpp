38a63ac6affc15ac772543517704a7935e2ffae6
Montoya-27 <164410888+Montoya-27@users.noreply.github.com>
2024-05-08 12:38:07 -0600

    Ejemplos de for

--- main (1).cpp
+++ main (1).cpp
@@ -1,20 +1,23 @@
 /******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
-#include <iostream>
-using namespace std;
 
-int main(){
-    int i= 1;
-    while (i<10){
-        cout<< i << "\n";
-        i++;
+#include <iostream>
+
+using namespace std;
+int main()
+{
+   int n; 
+   cout <<"ingrese un numero positivo entero";
+   cin >> n;
+    int i = 0;
+    for (i=2; i <= n; i+=2)
+    {
+        cout << i <<"\n"; 
     }
     return 0;
 }
-
--- main.cpp
+++ main.cpp
@@ -1,20 +1,31 @@
 /******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
+
 #include <iostream>
 using namespace std;
-
-int main(){
-    int i= 1;
-    while (i<13){
-        cout<< i <<"x12 = " << i*12 <<endl;
-        i++;
+int main()
+{
+    char opcion; 
+    char variable='x';
+    
+    do 
+    {
+    cout <<"Menu"<<"\n";
+    cout <<"A Hamburguesa"<<"\n";
+    cout <<"B Polo campero"<<"\n";
+    cout <<"C Camarones"<<"\n";
+    cout <<"Seleccione una opcion:"<<"\n";
+    cin >> opcion;
+    cout <<"opcion seleccionada"<< opcion <<"\n";
+    
     }
+    while (opcion !=variable);
+    
     return 0;
 }
 
